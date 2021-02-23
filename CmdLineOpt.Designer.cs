af1aebe85595745961f1bcf5eacac71c2c4e01e9
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-02-23 16:39:44 +0000

    Add warning for file paths

--- CmdLineOpt.Designer.cs
+++ CmdLineOpt.Designer.cs
@@ -31,13 +31,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.CmdBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Location = new System.Drawing.Point(12, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(344, 19);
             this.label1.TabIndex = 5;
@@ -47,7 +48,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(362, 43);
+            this.button1.Location = new System.Drawing.Point(420, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 38);
             this.button1.TabIndex = 4;
@@ -60,17 +61,29 @@
             this.CmdBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CmdBox.Location = new System.Drawing.Point(11, 12);
             this.CmdBox.Name = "CmdBox";
-            this.CmdBox.Size = new System.Drawing.Size(435, 25);
+            this.CmdBox.Size = new System.Drawing.Size(493, 25);
             this.CmdBox.TabIndex = 3;
             this.CmdBox.Text = "Preview";
             this.CmdBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(402, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File paths with spaces require the path to be surrounded with \"\".";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // CmdLineOpt
             // 
             this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(461, 93);
+            this.ClientSize = new System.Drawing.Size(516, 93);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.CmdBox);
@@ -88,5 +101,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox CmdBox;
+        private System.Windows.Forms.Label label2;
     }
 }
